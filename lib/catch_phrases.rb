f36e624575420e88da436d29370b79b1312d6d27
masterful-principle-0059 <william.neal83@gmail.com>
2020-09-01 14:06:33 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,20 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = "It's-a me, Mario!"
   puts phrase
 end
 
 def toadstool
+  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
+end
+
+def link
+  heart_cont = "It's Dangerous To Go Alone! Take This."
+  puts heart_cont
+end
+
+def all_phrases
+  puts link
+  puts mario
+  puts toadstool
 end
