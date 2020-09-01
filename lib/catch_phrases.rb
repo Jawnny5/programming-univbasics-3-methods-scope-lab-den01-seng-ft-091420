0f3e4a29f72ac8d2d03e14ddc754c3eecbafe627
masterful-principle-0059 <william.neal83@gmail.com>
2020-09-01 14:05:09 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,3 +7,14 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  heart_cont = "It's dangerous to go alone, take this!"
+  puts heart_cont
+end
+
+def all_phrases
+  puts link
+  puts mario
+  puts toadstool
+end
